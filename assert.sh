06d05b1e9df2ecd9504c4a63818f2f4f5e7873b7
brownman <brownman556@gmail.com>
2015-02-08 18:28:16 +0200

    group cmd to funcs

--- assert.sh
+++ assert.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 trace(){
-        echo 1>&2  "$@"
+echo 1>&2  "$@"
 }
 
 commander() { 
-    local args=($@)
-    local cmd="${args[@]}"
-    trace  "[cmd] $cmd"
-    eval "$cmd"
+local args=($@)
+local cmd="${args[@]}"
+trace  "[cmd] $cmd"
+eval "$cmd"
 }
 
- dir_root=$( cd $(dirname $0); echo $PWD; )
- cd $(npm -g root)/se-interpreter
- npm ls
- npm install -g
- npm link
- npm ls
-
+set_env(){
+dir_root=$( cd $(dirname $0); echo $PWD; )
 file_ok=$dir_root/ok.json
 file_err=$dir_root/err.json
+}
 
-
+ensure(){
 test -f $file_ok
 test -f $file_err
+}
 
+steps(){
+set_env
+ensure
+pre_run
+run
+}
 
+pre_run(){
+cd $(npm -g root)/se-interpreter
+npm ls
+npm install -g
+npm link
+npm ls
+}
+
+run(){
 ( commander node interpreter.js  $file_ok )
 echo "============================== $?"
-#commander sleep 3
-
 ( commander node interpreter.js  $file_err )
 echo "============================== $?"
+}
+
+
