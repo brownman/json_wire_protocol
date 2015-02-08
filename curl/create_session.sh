c9a9ad2b12bf568d62c5a82ed3d3755b31ab6e2f
brownman <brownman556@gmail.com>
2015-02-08 19:57:44 +0200

    curl: set browser

--- curl/create_session.sh
+++ curl/create_session.sh
@@ -2,15 +2,17 @@
 
 #- RemoteWebDriver instances should connect to: http://127.0.0.1:4443/wd/hub
 #- Started SocketListener on 0.0.0.0:4443
-browser=chrome
+
 
 while true;do
   ( netstat -ntlp | grep 4444 ) && break || { echo selenium-server still down; } 
   sleep 1
 done
 
-echo POST /session {"desiredCapabilities":{"browserName":"$browser","version":"","javascriptEnabled":true,"platform":"ANY"}}
+echo POST /session {"desiredCapabilities":{"browserName":"chrome","version":"","javascriptEnabled":true,"platform":"ANY"}}
+curl -i --data {"desiredCapabilities":{"browserName":"chrome","version":"","platform":"ANY","javascriptEnabled":true}}\' http://localhost:4444/wd/hub/session
 
-curl -i --data \'{"desiredCapabilities":{"browserName":"$browser","version":"","platform":"ANY","javascriptEnabled":true}}\' http://localhost:4444/wd/hub/session
+echo POST /session {"desiredCapabilities":{"browserName":"firefox","version":"","javascriptEnabled":true,"platform":"ANY"}}
+curl -i --data {"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}\' http://localhost:4444/wd/hub/session
 
 
