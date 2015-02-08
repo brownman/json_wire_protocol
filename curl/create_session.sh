a8e75ea4f047144e1c3bb26ca1fc29c62c5c9c1e
brownman <brownman556@gmail.com>
2015-02-08 19:17:33 +0200

    ensure selenium-server is alive
    + listen on port 4443

--- curl/create_session.sh
+++ curl/create_session.sh
@@ -1,3 +1,10 @@
+#!/bin/bash  
+
+#- RemoteWebDriver instances should connect to: http://127.0.0.1:4443/wd/hub
+#- Started SocketListener on 0.0.0.0:4443
+
+netstat -ntlp | grep 4444
+
 echo POST /session {"desiredCapabilities":{"browserName":"firefox","version":"","javascriptEnabled":true,"platform":"ANY"}}
 
 curl -i --data '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}' http://localhost:4444/wd/hub/session
