6daa87dd877a70430ba6283946f2ffc55ab129ca
brownman <brownman556@gmail.com>
2015-02-08 20:34:07 +0200

    Update create_session.sh

--- curl/create_session.sh
+++ curl/create_session.sh
@@ -12,7 +12,7 @@ done
 echo POST /session {"desiredCapabilities":{"browserName":"chrome","version":"","javascriptEnabled":true,"platform":"ANY"}}
 curl -i --data '{"desiredCapabilities":{"browserName":"chrome","version":"","platform":"ANY","javascriptEnabled":true}}' http://localhost:4444/wd/hub/session
 
-echo POST /session {"desiredCapabilities":{"browserName":"firefox","version":"","javascriptEnabled":true,"platform":"ANY"}}
-curl -i --data '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}' http://localhost:4444/wd/hub/session
+#echo POST /session {"desiredCapabilities":{"browserName":"firefox","version":"","javascriptEnabled":true,"platform":"ANY"}}
+#curl -i --data '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}' http://localhost:4444/wd/hub/session
 
 
