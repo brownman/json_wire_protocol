99cd4b92931a51b3b56bc70975418d30ea135e68
brownman <brownman556@gmail.com>
2015-02-09 17:27:33 +0200

    Update install_selenium.sh

--- sh/install_selenium.sh
+++ sh/install_selenium.sh
@@ -17,10 +17,10 @@ install_selenium_2(){
 }
 
 install_chrome(){
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-  sudo sh -c echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list
-  sudo apt-get update
-  sudo apt-get install google-chrome-stable
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update -qq
+sudo apt-get install -qq google-chrome-stable
 }
 
 install_selenium(){
