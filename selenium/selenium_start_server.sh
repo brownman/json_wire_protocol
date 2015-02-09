1aa3ae0f0af68afe96561f68272f7b1433838b54
brownman <brownman556@gmail.com>
2015-02-09 14:48:02 +0200

    Update selenium_start_server.sh

--- selenium/selenium_start_server.sh
+++ selenium/selenium_start_server.sh
@@ -1,3 +1,3 @@
-chrome_driver=$(which chromedriver)
-selenium_jar=selenium-server-standalone-2.44.0.jar
-java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$chrome_driver
+commander test -f $chrome_driver
+commander test -f $selenium_jar
+commander java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$chrome_driver
