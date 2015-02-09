8209b207c6c49f497e82a8d70615857d8af35120
brownman <brownman556@gmail.com>
2015-02-09 14:12:00 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -9,6 +9,21 @@ source $dir_root/cfg/config.cfg
 source $dir_root/cfg/vars.cfg
 }
 
+imported(){
+
+  sudo apt-get install -y -q curl
+   npm install -g se-interpreter
+
+  wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
+  java -jar selenium-server-standalone-2.25.0.jar > /dev/null 2>&1 &
+
+ chmod u+x ./curl/create_session.sh
+ bash -c ./curl/create_session.sh
+
+ chmod u+x ./assert.sh
+ bash -c ./assert.sh
+}
+
 run(){
 commander echo hello world
 commander blllllaaaaa_errrrr
