0bd32f2afcf98465ee232dafb7a00ce194e93812
brownman <brownman556@gmail.com>
2015-02-09 15:29:06 +0200

    Update selenium_start_server.sh

--- sh/selenium_start_server.sh
+++ sh/selenium_start_server.sh
@@ -1,3 +1,12 @@
 ensure test -f $chrome_driver
 ensure test -f $selenium_jar
 commander java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$chrome_driver
+
+
+
+while true;do
+  ( netstat -ntlp | grep 4444 ) && break || { trace selenium-server still down; } 
+  sleep 1
+done
+
+trace Selenium is alive !
