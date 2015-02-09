c281572a416cc4bdfff06d243ca84b59e67ee309
brownman <brownman556@gmail.com>
2015-02-09 18:25:26 +0200

    Update curl_create_session.sh

--- sh/curl_create_session.sh
+++ sh/curl_create_session.sh
@@ -2,17 +2,17 @@
 
 #- RemoteWebDriver instances should connect to: http://127.0.0.1:4443/wd/hub
 #- Started SocketListener on 0.0.0.0:4443
-
+path=session
 
 use_chrome(){
 echo POST /session {"desiredCapabilities":{"browserName":"chrome","version":"","javascriptEnabled":true,"platform":"ANY"}}
-curl -i --data '{"desiredCapabilities":{"browserName":"chrome","version":"","platform":"ANY","javascriptEnabled":true}}' $HTTP_INTERFACE
+curl -i --data '{"desiredCapabilities":{"browserName":"chrome","version":"","platform":"ANY","javascriptEnabled":true}}' $HTTP_INTERFACE/$path
 #http://localhost:4444/wd/hub/session
 }
 
 use_firefox(){
 echo POST /session {"desiredCapabilities":{"browserName":"firefox","version":"","javascriptEnabled":true,"platform":"ANY"}}
-curl -i --data '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}' $HTTP_INTERFACE
+curl -i --data '{"desiredCapabilities":{"browserName":"firefox","version":"","platform":"ANY","javascriptEnabled":true}}' $HTTP_INTERFACE/$path
 #http://localhost:4444/wd/hub/session
 }
 
