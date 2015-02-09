f0b897da03b3fb8cfa6929b39bd8a2235f994140
brownman <brownman556@gmail.com>
2015-02-09 14:52:03 +0200

    Update install_selenium_server.sh

--- selenium/install_selenium_server.sh
+++ selenium/install_selenium_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
+wget $selenium_server_url
 #java -jar selenium-server-standalone-2.25.0.jar > /dev/null 2>&1 &
