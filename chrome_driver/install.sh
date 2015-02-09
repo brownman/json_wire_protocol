0f02ba5c56a4012b466d2654a0ad05ee940c9b72
brownman <brownman556@gmail.com>
2015-02-09 14:00:36 +0200

    Update install.sh

--- chrome_driver/install.sh
+++ chrome_driver/install.sh
@@ -1,10 +1,14 @@
 CHROMEDRIVER_LINUX_URL='http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux32.zip'
 SELENIUM=$HOME/SELENIUM
 
-mkdir SELENIUM
+#location
+mkdir -p $SELENIUM
 cd $SELENIUM
 
+#download + extract
 wget $CHROMEDRIVER_LINUX_URL 
 unzip chromedriver*.zip
 rm chromedriver*.zip
+
+#ensure file exist
 ls -l chrom*
