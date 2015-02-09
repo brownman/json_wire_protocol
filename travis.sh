b5c003dc74086c821bf736430fe9f4e078896e76
brownman <brownman556@gmail.com>
2015-02-09 14:39:06 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,34 +3,35 @@
 
 
 export dir_root=$( cd `dirname $0`; echo $PWD; )
+ensure(){
+  test -v SELENIUM
+}
 
 set_env(){
 source $dir_root/cfg/config.cfg
 source $dir_root/cfg/vars.cfg
 }
 
-imported(){
+fix_permission(){
+  commander chmod u+x *.sh . -R
+}
 
-  sudo apt-get install -y -q curl
-   npm install -g se-interpreter
-
-  wget http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
-  java -jar selenium-server-standalone-2.25.0.jar > /dev/null 2>&1 &
-
- chmod u+x ./curl/create_session.sh
- bash -c ./curl/create_session.sh
-
- chmod u+x ./assert.sh
- bash -c ./assert.sh
+install(){
+   commander sudo apt-get install -y -q curl
+   commander npm install -g se-interpreter
 }
 
 run(){
-commander echo hello world
-commander blllllaaaaa_errrrr
+ while read line;do
+   commander "$line"
+ done < $file_steps
 }
 
 steps(){
 set_env
+ensure
+fix_permission
+install
 run
 }
 
