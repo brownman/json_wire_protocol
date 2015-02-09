2bc39259e9b6bd42d708d05b8f57a37846fd383b
brownman <brownman556@gmail.com>
2015-02-09 18:16:40 +0200

    Update selenium_start_server.sh

--- sh/selenium_start_server.sh
+++ sh/selenium_start_server.sh
@@ -1,3 +1,6 @@
+
+
+commander cd $dir_selenium
 ensure test -f $chrome_driver
 ensure test -f $selenium_jar
 commander "java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$chrome_driver &"
