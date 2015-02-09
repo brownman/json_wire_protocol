9ecdcc197c1320c3e709e3e67d4f150f360f488d
brownman <brownman556@gmail.com>
2015-02-09 15:55:01 +0200

    Update assertTitle.sh

--- sh/assertTitle.sh
+++ sh/assertTitle.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-trace(){
-echo 1>&2  "$@"
-}
-
-commander() { 
-local args=($@)
-local cmd="${args[@]}"
-trace  "[cmd] $cmd"
-eval "$cmd" || { trace exiting 1; exit 1; }
-}
 
 set_env(){
-dir_root=$( cd $(dirname $0); echo $PWD; )
-file_ok=$dir_root/ok.json
-file_err=$dir_root/err.json
+file_ok=$dir_root/json/ok.json
+file_err=$dir_root/json/err.json
 commander cd $(npm -g root)/se-interpreter
+commander echo $PWD
 }
 
-ensure(){
-test -f $file_ok
-test -f $file_err
-commander echo $PWD
-ls -l node_modules
+ensure_stuff(){
+ensure test -f $file_ok
+ensure test -f $file_err
+ensure ls -l node_modules
 }
 
  
 
 run(){
-
 commander node interpreter.js  $file_ok 
 echo "============================== $?"
 commander node interpreter.js  $file_err 
@@ -36,9 +24,9 @@ echo "============================== $?"
 
 
 steps(){
-set_env
-ensure
-run
+commander set_env
+commander ensure_stuff
+commander run
 }
 
 
