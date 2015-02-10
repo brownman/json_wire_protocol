5c774c51d0fc4ce30a370a7b7b6218b56a66affb
brownman <brownman556@gmail.com>
2015-02-10 17:01:27 +0200

    Update install_chromedriver.sh

--- sh/install_chromedriver.sh
+++ sh/install_chromedriver.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
+  CHROMEDRIVER_VERSION="2.10"
+
+trace 'should be compatible with   CHROME_REVISION="290620"'
 #ref:
 #https://github.com/jarib/selenium-travis/blob/master/install.sh
 commander curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 commander unzip chromedriver_linux64.zip
 
-commander mv chromedriver chrome-linux/chromedriver
-commander chmod +x chrome-linux/chromedriver
+#commander mv chromedriver chrome-linux/chromedriver
+commander chmod +x chromedriver
 
+mv chromedriver $dir_tmp_bin
