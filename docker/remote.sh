5d9f5faba2ebbe11351b773f9e3ec358039ba6e1
brownman <brownman556@gmail.com>
2015-02-12 20:01:41 +0200

    Update remote.sh
    [skip ci]

--- docker/remote.sh
+++ docker/remote.sh
@@ -1,11 +1,30 @@
 
+set_env0(){
+host=$HOST
+port=$PORT
+path='/'
+}
 
-gitlab_ci_runner@046ccd32806b:~/.nvm/v0.10.33/lib/node_modules/se-interpreter$ cat config.cfg 
+set_env1(){
 host=$SELENIUM_SERVER_PORT_4444_TCP_ADDR
 port=$SELENIUM_SERVER_PORT_4444_TCP_PORT
 path='/wd/hub'
-file1='examples/tests/assertTitle.json'
+}
 
-cmd="node ./interpreter.js --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=chrome $file1""
+set_env(){
+file_ok=$dir_root/json/ok.json
+file_err=$dir_root/json/err.json
+browser=chrome
+cmd_se="node ./interpreter.js   --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=$browser"
+}
+
+test1(){
+local file=$1
+test -f $file
+cmd="node ./interpreter.js --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=chrome $file"
 echo $cmd
 $cmd
+}
+
+test1 $file_ok
+test1 $file_err
