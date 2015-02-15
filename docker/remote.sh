615b69d0e4ab25de0f16ceb1a1c8359eee424de2
brownman <brownman556@gmail.com>
2015-02-15 16:13:39 +0200

    Update remote.sh

--- docker/remote.sh
+++ docker/remote.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
 
-set_env0(){
-host=$HOST
-port=$PORT
-path='/'
+set -u
+set -e
+commander test -v dir_root
+commander test -d "$dir_root"
+
+set_env(){
+file_ok=$dir_root/json/ok.json
+file_err=$dir_root/json/err.json
+browser=chrome
+cmd_se="node ./interpreter.js   --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=$browser"
 }
 
 set_env1(){
@@ -11,12 +18,12 @@ port=$SELENIUM_SERVER_PORT_4444_TCP_PORT
 path='/wd/hub'
 }
 
-set_env(){
-file_ok=$dir_root/json/ok.json
-file_err=$dir_root/json/err.json
-browser=chrome
-cmd_se="node ./interpreter.js   --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=$browser"
+set_env2(){
+host=$HOST
+port=$PORT
+path='/'
 }
+
 
 test1(){
 local file=$1
@@ -26,5 +33,11 @@ echo $cmd
 $cmd
 }
 
+
+steps(){
+set_env
+set_env1
 test1 $file_ok
 test1 $file_err
+}
+steps
