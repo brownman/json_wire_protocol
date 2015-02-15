3f31e78a8810e72b5de851549a69864a863251d0
root <root@docker0.linnovate.net>
2015-02-15 17:28:25 +0200

    fix: docker.sh

--- docker/remote.sh
+++ docker/remote.sh
@@ -2,22 +2,29 @@
 
 set -u
 set -e
-command npm &>/dev/null || { trace 'npm not found'; exit 0; }
+#command npm &>/dev/null || { trace 'npm not found'; exit 0; }
+commander npm -v
 commander test -v dir_root
 commander test -d "$dir_root"
 
+intro(){
+commander 'env | grep 4444'
+}
 set_env(){
 file_ok=$dir_root/json/ok.json
 file_err=$dir_root/json/err.json
 file_interpreter=$(npm -g root)/se-interpreter/interpreter.js
 browser=chrome
-cmd_se="node $file_interpreter   --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=$browser"
+
+
 }
 
 set_env1(){
-host=$SELENIUM_SERVER_PORT_4444_TCP_ADDR
-port=$SELENIUM_SERVER_PORT_4444_TCP_PORT
-path='/wd/hub'
+
+host=$SELENIUM_PORT_4444_TCP_ADDR
+port=$SELENIUM_PORT_4444_TCP_PORT
+path='wd/hub'
+cmd_se="node $file_interpreter   --driver-host=$host --driver-port=$port --driver-path='$path' --browser-browserName=$browser"
 }
 
 set_env2(){
@@ -37,9 +44,11 @@ commander "$cmd_se $file"
 
 
 steps(){
+intro
 set_env
 set_env1
-test1 $file_ok
-test1 $file_err
+commander "curl $host:$port/$path/status"
+commander "test1 $file_ok"
+commander "test1 $file_err"
 }
 steps
