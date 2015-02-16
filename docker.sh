067ee815f735f77b1841694be813cd37a8d17a24
root <root@docker0.linnovate.net>
2015-02-16 14:18:22 +0200

    [remote.sh] - accept input file or string:test for running default tests

--- docker.sh
+++ docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -u
 
-
+file_json="${1:-}"
+if [ "$file_json" != 'test' ];then
+( test -v file_json && { test -f $file_json ; } && { trace "[file exist] $file"; } ) || { trace "[NO FILE INPUT] run default tests"; exit 1; }
+else
+  trace '[default] showing default tests: assertOK+assertERR'
+fi
 export dir_root=$( cd `dirname $0`; echo $PWD; )
 ######temp:
 
@@ -34,7 +39,7 @@ commander ensure_stuff
 commander fix_permission
 #commander install
 #commander run
-commander bash -c $dir_root/docker/remote.sh
+commander bash -c $dir_root/docker/remote.sh $file_json
 }
 
 
