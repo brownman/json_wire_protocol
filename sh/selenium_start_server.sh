8cb463fb3b39a3d21235fddae7d0c907f7870de1
brownman <brownman556@gmail.com>
2015-07-30 18:13:26 +0300

    Update selenium_start_server.sh

--- sh/selenium_start_server.sh
+++ sh/selenium_start_server.sh
@@ -7,7 +7,7 @@ ensure test -f $chrome_driver
 ensure test -f $selenium_jar
 ensure which chromedriver
 
-( commander "sudo java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$(which chromedriver)" || { trace kill process; kill 0; }  )&
+( commander "sudo java -jar $selenium_jar -timeout=20 -browserTimeout=60x -Dwebdriver.chrome.driver=$(which chromedriver)" || { trace kill process; kill 0; }  &)&
 
 
 trace Selenium is alive !
